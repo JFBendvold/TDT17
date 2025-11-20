ce83943680c11aea7fe5ab4ea7243520193906dc
JFBendvold <j.f.bendvold@gmail.com>
2025-11-20 12:47:33 +0100

    Trained at imgsz 640 and 1280

--- RT-DETR.py
+++ RT-DETR.py
@@ -1,2 +1,15 @@
 # RT-DETR
 from ultralytics import RTDETR
+
+data_path = "/datasets/tdt4265/Poles2025/roadpoles_v1/data.yaml"
+
+model = RTDETR('./rtdetr/train/weights/best.pt')
+
+model.train(
+    data=data_path,
+    epochs=100,
+    imgsz=1280,
+    batch=8,
+    lr0=0.001,
+    project='rtdetr_1280',
+)
