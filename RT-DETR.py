261100eabd8dffb01cd4867e16afb64405ddde29
JFBendvold <j.f.bendvold@gmail.com>
2025-11-20 14:41:00 +0100

    Train on both datasets

--- RT-DETR.py
+++ RT-DETR.py
@@ -1,13 +1,14 @@
 # RT-DETR
 from ultralytics import RTDETR
 
-data_path = "/datasets/tdt4265/Poles2025/roadpoles_v1/data.yaml"
+#data_path = "/datasets/tdt4265/Poles2025/roadpoles_v1/data.yaml"
+data_path = "./cluster_combined_data.yaml"
 
-model = RTDETR('./rtdetr/train/weights/best.pt')
+model = RTDETR('rtdetr_1280/train3/weights/best.pt')
 
 model.train(
     data=data_path,
-    epochs=100,
+    epochs=10,
     imgsz=1280,
     batch=8,
     lr0=0.001,
