3885f402b44b398c8bd7afff03497b05200ef681
flippi321 <chribrev@stud.ntnu.no>
2025-11-20 12:21:24 +0100

    Made the YOLO script automatically load the best run and create a file with the best models evaluation metrics (on val dataset, will be test when it's out)

--- YOLO.py
+++ YOLO.py
@@ -11,12 +11,12 @@ else:       data_root = "/datasets/tdt4265/Poles2025"
 
 data_path       = os.path.join(data_root, dataset, "data.yaml")
 test_set_path   = os.path.join(data_root, dataset, "test", "images")
-
+output_dir      = "yolo/nano"
 
 
 # ----- Train Model ----- 
 model = YOLO("yolo11n.pt")
-results = model.train(data=data_path, epochs=100, imgsz=1280, batch=8, project="yolo", lr0=1e-3)
+results = model.train(data=data_path, epochs=250, imgsz=1280, batch=8, project=output_dir, lr0=1e-3)
 
 
 
@@ -26,8 +26,33 @@ all_imgs = [os.path.join(test_set_path, f) for f in os.listdir(test_set_path)]
 results = model.predict(
     source=all_imgs[5:],    # We sample the first 5 imgs
     save=True,              # saves images with boxes
-    project="yolo/out",
+    project=os.path.join(output_dir, "out"),
     exist_ok=True           # don't create new numbered folder each run
 )
 
 print("Saved annotated images to: runs/export/sample_preds")
+
+
+
+# ----- Evaluation on Testset -----
+
+best_model_path= os.path.join(output_dir, "train", "weights", "best.pt")
+best_model = YOLO(best_model_path)
+
+metrics = best_model.val(
+    data=data_path,
+    split="val", 
+    imgsz=1280,
+    batch=8
+)
+
+metrics_path = os.path.join(output_dir, "out", "test_metrics.txt")
+
+with open(metrics_path, "w") as f:
+    f.write("--- Test Set Performance ---\n")
+    f.write(f"Precision:       {metrics.box.mp:.6f}\n")
+    f.write(f"Recall:          {metrics.box.mr:.6f}\n")
+    f.write(f"mAP@50:          {metrics.box.map50:.6f}\n")
+    f.write(f"mAP@0.5:0.95:    {metrics.box.map:.6f}\n")
+
+print(f"Saved metrics to: {metrics_path}")
